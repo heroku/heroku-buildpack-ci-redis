8e118c3d738fd9945f7ec8df75d20ce3a7f568aa
Matthew Blewitt <blewitt.matt@gmail.com>
2024-07-04 17:45:49 +0100

    feat: overhaul - add tls, version support
    This is a pretty huge monster of a change and needs to be split up.
    
    In summary, to align with the Redis Version Support Policy from Heroku,
    we need to drop all versions < 6.2.
    
    Additionally, we need to support TLS to align with platform standards.
    This is enabled by passing in `REDIS_CI_TLS` into the environment, which
    will start a TLS-enabled `redis-server` instead of a plaintext one. This
    allows for closer parity with production in CI.

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,11 @@ FROM $BASE_IMAGE
 USER root
 
 ARG REDIS_VERSION
+ARG REDIS_CI_TLS
 
 RUN mkdir -p /app /cache /env
 RUN [ -z "${REDIS_VERSION}" ] || echo "${REDIS_VERSION}" > /env/REDIS_VERSION
+RUN [ -z "${REDIS_CI_TLS}" ] || echo "${REDIS_CI_TLS}" > /env/REDIS_CI_TLS
 COPY . /buildpack
 # Sanitize the environment seen by the buildpack, to prevent reliance on
 # environment variables that won't be present when it's run by Heroku CI.
--- bin/test.sh
+++ bin/test.sh
@@ -7,21 +7,30 @@ set -euo pipefail
 STACK="${1}"
 BASE_IMAGE="heroku/${STACK/-/:}-build"
 OUTPUT_IMAGE="redis-test-${STACK}"
+REDIS_CI_TLS=${REDIS_CI_TLS:-}
 
 echo "Building buildpack on stack ${STACK}...with redis version ${REDIS_VERSION}"
 
 docker build \
+    --no-cache \
     --build-arg "BASE_IMAGE=${BASE_IMAGE}" \
     ${REDIS_VERSION:+--build-arg "REDIS_VERSION=${REDIS_VERSION}"} \
+    ${REDIS_CI_TLS:+--build-arg "REDIS_CI_TLS=${REDIS_CI_TLS}"} \
     -t "${OUTPUT_IMAGE}" \
     .
 
 echo "Checking redis-server presence and version..."
 
+if [ -n "$REDIS_CI_TLS" ]; then
+    REDIS_TLS_ARGS="--tls --insecure"
+else
+    REDIS_TLS_ARGS=""
+fi
+
 # Redis <4 does not support connection URLs so REDIS_URL has to be parsed:
 # https://stackoverflow.com/questions/38271281/can-i-use-redis-cli-with-a-connection-url
-REDIS_CONNECTION_ARGS="\$(echo \${REDIS_URL} | sed 's_redis://\(.*\):\(.*\)@\(.*\):\(.*\)/_-h \3 -p \4 -a \2_')"
-TEST_COMMAND="source .profile.d/redis.sh && sleep 1 && redis-cli ${REDIS_CONNECTION_ARGS} info | grep redis_version:${REDIS_VERSION:-}"
+REDIS_CONNECTION_ARGS="\$(echo \${REDIS_URL} | sed 's_rediss\{0,1\}://\(.*\):\(.*\)@\(.*\):\(.*\)/_-h \3 -p \4 -a \2_')"
+TEST_COMMAND="source .profile.d/redis.sh && sleep 1 && redis-cli ${REDIS_TLS_ARGS} ${REDIS_CONNECTION_ARGS} info | grep redis_version:${REDIS_VERSION:-}"
 docker run --rm -t "${OUTPUT_IMAGE}" bash -c "${TEST_COMMAND}"
 
 echo "Success!"
