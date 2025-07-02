ee123ee4316133c4941b0b50c3757a3e8e0af08b
Matt Blewitt <118200221+mble-sfdc@users.noreply.github.com>
2025-07-02 12:44:00 +0100

    feat: migrate to valkey, no modules
    This commit migrates to valkey and enforces version support according to
    the [relevant Dev Center
    article](https://devcenter.heroku.com/articles/heroku-redis#version-support-and-legacy-infrastructure).
    
    No module support yet, will be a follow up.
    
    Ref: W-18942478

--- bin/test.sh
+++ bin/test.sh
@@ -8,7 +8,7 @@ STACK="${1}"
 BASE_IMAGE="heroku/${STACK/-/:}-build"
 OUTPUT_IMAGE="redis-test-${STACK}"
 
-echo "Building buildpack on stack ${STACK}...with redis version ${REDIS_VERSION}"
+echo "Building buildpack on stack ${STACK}...with valkey version ${REDIS_VERSION}"
 
 docker build \
     --build-arg "BASE_IMAGE=${BASE_IMAGE}" \
@@ -21,7 +21,7 @@ echo "Checking redis-server presence and version..."
 # Redis <4 does not support connection URLs so REDIS_URL has to be parsed:
 # https://stackoverflow.com/questions/38271281/can-i-use-redis-cli-with-a-connection-url
 REDIS_CONNECTION_ARGS="\$(echo \${REDIS_URL} | sed 's_redis://\(.*\):\(.*\)@\(.*\):\(.*\)/_-h \3 -p \4 -a \2_')"
-TEST_COMMAND="source .profile.d/redis.sh && sleep 1 && redis-cli ${REDIS_CONNECTION_ARGS} info | grep redis_version:${REDIS_VERSION:-}"
+TEST_COMMAND="source .profile.d/valkey.sh && sleep 1 && valkey-cli ${REDIS_CONNECTION_ARGS} info server | grep valkey_version:${REDIS_VERSION:-}"
 docker run --rm -t "${OUTPUT_IMAGE}" bash -c "${TEST_COMMAND}"
 
 echo "Success!"
